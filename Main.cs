c3e89fd748f026fcb8d064b109aecde587b0ea59
Nether <enzonether@gmail.com>
2024-03-08 22:19:32 -0300

    Create Main.cs

---
+++ Main.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Escolha a operação : ");
+        Console.WriteLine("1 - Adição");
+        Console.WriteLine("2 - Subtração");
+        Console.WriteLine("3 - Multiplicação");
+        Console.WriteLine("4 - Divisão");
+        int operacao = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Escolha o primeiro numero do calculo : ");
+        float num1 = float.Parse(Console.ReadLine());
+        Console.WriteLine("Escolha o segundo numero do calculo : ");
+        float num2 = float.Parse(Console.ReadLine());
+        float resultado = 0;
+
+        switch (operacao)
+        {
+            case 1: resultado = num1 + num2; break;
+            case 2: resultado = num1 - num2; break;
+            case 3: resultado = num1 * num2; break;
+            case 4: resultado = num1 / num2; break;
+        }
+
+        Console.WriteLine($"O resultado da sua operação foi {resultado}");
+    }
+}
